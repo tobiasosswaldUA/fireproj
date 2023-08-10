5e407876a2497f4d95234c7c96b7b3f00e653b16
Eduardo Martins <51093877+headdu@users.noreply.github.com>
2023-08-10 21:41:15 +0100

    Add public to middleware

--- firestorm/src/middleware.ts
+++ firestorm/src/middleware.ts
@@ -37,7 +37,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    "/((?!api|_next|.*\\..*).*)",
+    "/((?!api|_next|public|.*\\..*).*)",
     // Optional: only run on root (/) URL
     // '/'
   ],
