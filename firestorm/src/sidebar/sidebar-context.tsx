fdfc57ed591ed94a717785f0553bbfc8d6814f87
Eduardo Martins <51093877+headdu@users.noreply.github.com>
2023-08-21 21:37:50 +0100

    feat(sidebar): Make default state as closed

--- firestorm/src/sidebar/sidebar-context.tsx
+++ firestorm/src/sidebar/sidebar-context.tsx
@@ -2,12 +2,12 @@
 import { createContext, useState } from "react";
 
 export const SidebarContext = createContext({
-  show: true,
+  show: false,
   updateShow: (status: boolean) => {},
 });
 
 const SidebarContextProvider = ({ children }: React.PropsWithChildren) => {
-  const [show, updateShow] = useState<boolean>(true);
+  const [show, updateShow] = useState<boolean>(false);
 
   return (
     <SidebarContext.Provider value={{ show, updateShow }}>
