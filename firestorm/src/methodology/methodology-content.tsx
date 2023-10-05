d1956afe834996597634e714cee32902bf4c0a53
Eduardo Martins <51093877+headdu@users.noreply.github.com>
2023-10-05 17:01:47 +0100

    fix(methodology): Fix formatting

--- firestorm/src/methodology/methodology-content.tsx
+++ firestorm/src/methodology/methodology-content.tsx
@@ -3,32 +3,54 @@ import { useTranslations } from "next-intl";
 
 const MethodologyContent = () => {
   const t = useTranslations("Methodology");
+
   return (
-    <div className="p-5">
-      <h1>{t("title")}</h1>
-      <p>{t("general_content")}</p>
-      <h2 className="mt-4">{t("fires")}</h2>
-      <p>{t("lorem_ipsum")}</p>
-      <h2 className="mt-4">{t("models")}</h2>
+    <div className="container py-5">
+      <h1
+        dangerouslySetInnerHTML={{
+          __html: t.raw("title"),
+        }}
+      ></h1>
+      <p dangerouslySetInnerHTML={{ __html: t.raw("general_content") }}></p>
+      <h2
+        className="mt-4"
+        dangerouslySetInnerHTML={{ __html: t.raw("fires") }}
+      ></h2>
+      <p dangerouslySetInnerHTML={{ __html: t.raw("lorem_ipsum") }}></p>
+      <h2
+        className="mt-4"
+        dangerouslySetInnerHTML={{ __html: t.raw("models") }}
+      ></h2>
       <div className="d-flex flex-column flex-lg-row gap-4">
-        <p>{t("models_description")}</p>
+        <p
+          dangerouslySetInnerHTML={{ __html: t.raw("models_description") }}
+        ></p>
         <img
           src="http://gestao.tncreate.pt/upload/iqa/6rzyuWc8/img/content/Dewu5jYV/thumbs/esquema_previsao_web_page.png"
           alt="WRF-CHIMERE"
         />
       </div>
-      <h2 className="mt-4">{t("air_quality")}</h2>
-      <p>{t.raw("air_quality_description")}</p>
+      <h2
+        className="mt-4"
+        dangerouslySetInnerHTML={{ __html: t.raw("air_quality") }}
+      ></h2>
+      <p
+        dangerouslySetInnerHTML={{ __html: t.raw("air_quality_description") }}
+      ></p>
       <table className="w-100" border={1} cellPadding={2} cellSpacing={0}>
         <thead>
           <tr>
-            <td rowSpan={2} style={{ verticalAlign: "top" }}>
-              {t("pollutant")}
-            </td>
+            <td
+              rowSpan={2}
+              style={{ verticalAlign: "top" }}
+              dangerouslySetInnerHTML={{ __html: t.raw("pollutant") }}
+            ></td>
             <td colSpan={5}>
-              {t("index")}
+              {t.raw("index")}
               <br />
-              <span>{t("index_sub")}</span>
+              <span
+                dangerouslySetInnerHTML={{ __html: t.raw("index_sub") }}
+              ></span>
             </td>
           </tr>
           <tr>
@@ -41,7 +63,9 @@ const MethodologyContent = () => {
         </thead>
         <tbody>
           <tr>
-            <td>{t.raw("particles_2.5")}</td>
+            <td
+              dangerouslySetInnerHTML={{ __html: t.raw("particles_25") }}
+            ></td>
             <td>{t("0-10")}</td>
             <td>{t("10-20")}</td>
             <td>{t("20-25")}</td>
@@ -49,7 +73,9 @@ const MethodologyContent = () => {
             <td>{t("50")}</td>
           </tr>
           <tr>
-            <td>{t.raw("particles_10")}</td>
+            <td
+              dangerouslySetInnerHTML={{ __html: t.raw("particles_10") }}
+            ></td>
             <td>{t("0-20")}</td>
             <td>{t("20-40")}</td>
             <td>{t("40-50")}</td>
@@ -57,7 +83,7 @@ const MethodologyContent = () => {
             <td>{t("100")}</td>
           </tr>
           <tr>
-            <td>{t.raw("nitrogen")}</td>
+            <td dangerouslySetInnerHTML={{ __html: t.raw("nitrogen") }}></td>
             <td>{t("0-40")}</td>
             <td>{t("40-90")}</td>
             <td>{t("90-120")}</td>
@@ -65,7 +91,7 @@ const MethodologyContent = () => {
             <td>{t("230")}</td>
           </tr>
           <tr>
-            <td>{t.raw("ozone")}</td>
+            <td dangerouslySetInnerHTML={{ __html: t.raw("ozone") }}></td>
             <td>{t("0-50")}</td>
             <td>{t("50-100")}</td>
             <td>{t("100-130")}</td>
@@ -75,9 +101,11 @@ const MethodologyContent = () => {
         </tbody>
       </table>
 
-      <h2 className="mt-4">{t("smokestorm")}</h2>
+      <h2 className="mt-4">{t.raw("smokestorm")}</h2>
 
-      <p>{t.raw("smokestorm_description")}</p>
+      <p
+        dangerouslySetInnerHTML={{ __html: t.raw("smokestorm_description") }}
+      ></p>
 
       <video width="100%" height="" controls>
         <source
